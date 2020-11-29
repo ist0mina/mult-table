4b24c0d4f871374bcacf4967f4f0aa47219c8c1f
Истомина Наталья <istominans@it2g.ru>
2020-11-29 16:42:12 +0500

    Возможность начать игру заново в любом место прохождения

--- src/app/components/Board/Board.tsx
+++ src/app/components/Board/Board.tsx
@@ -1,56 +1,44 @@
-import React, { FC, memo, useState, useCallback, useEffect } from 'react';
-import { Button } from 'antd';
+import React, { FC, memo, useState, useCallback } from "react";
+import { Button } from "antd";
 
-import { TaskItem } from '../TaskItem';
-import { TaskModel } from '../../model/Task.model';
-import { generateTaskList, countResults } from '../../engine';
-import { StatisticPlay } from '../StatisticPlay';
+import { TaskItem } from "../TaskItem";
+import { TaskModel } from "../../model/Task.model";
+import { generateTaskList, countResults } from "../../engine";
+import { StatisticPlay } from "../StatisticPlay";
 
-import './Board.scss';
+import "./Board.scss";
 
 const COUNT_TASKS = 20;
 
 export const Board: FC = memo(() => {
-  const [taskList, setTaskList] = useState<TaskModel[]>([]);
-  const [userResults, setUserResults] = useState<{ [key: string]: boolean }>({});
-  const [isPlay, setIsPlay] = useState<boolean>(false);
+    const [taskList, setTaskList] = useState<TaskModel[]>([]);
+    const [userResults, setUserResults] = useState<{ [key: string]: boolean }>({});
+    const [isPlay, setIsPlay] = useState<boolean>(false);
 
-  const start = useCallback(() => {
-    setTaskList(generateTaskList(COUNT_TASKS));
-    setIsPlay(true);
-    setUserResults({});
-  }, []);
+    const titleBtn = isPlay ? "Начать заново" : "Начать";
 
-  useEffect(() => {
-    if (isPlay) {
-      setIsPlay(Object.keys(userResults).length < COUNT_TASKS);
-    }
-  }, [userResults, isPlay]);
+    const start = useCallback(() => {
+        setTaskList(generateTaskList(COUNT_TASKS));
+        setIsPlay(true);
+        setUserResults({});
+    }, []);
 
-  return (
-    <section className="container">
-      {isPlay && (
-        <StatisticPlay
-          right={countResults(true, userResults)}
-          wrong={countResults(false, userResults)}
-          counTasks={COUNT_TASKS}
-        />
-      )}
-      <div>
-        <Button disabled={isPlay} onClick={start}>
-          Начать
-        </Button>
-      </div>
-      <div>
-        {taskList.map((task) => (
-          <TaskItem
-            key={task.id}
-            task={task}
-            saveResult={(result: boolean) => setUserResults({ ...userResults, [task.id]: result })}
-            result={userResults[task.id]}
-          />
-        ))}
-      </div>
-    </section>
-  );
+    return (
+        <section className="container">
+            {isPlay && <StatisticPlay right={countResults(true, userResults)} wrong={countResults(false, userResults)} counTasks={COUNT_TASKS} />}
+            <div>
+                <Button onClick={start}>{titleBtn}</Button>
+            </div>
+            <div>
+                {taskList.map((task) => (
+                    <TaskItem
+                        key={task.id}
+                        task={task}
+                        saveResult={(result: boolean) => setUserResults({ ...userResults, [task.id]: result })}
+                        result={userResults[task.id]}
+                    />
+                ))}
+            </div>
+        </section>
+    );
 });
